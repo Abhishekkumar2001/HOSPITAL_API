6b0fd56e895d60101053e3e28657a13b1b40ed57
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-26 14:50:29 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,7 @@
+// Importing the express
 const express = require("express");
+
+
 const {
   registerDoctor,
   registerPatient,
@@ -9,26 +12,33 @@ const {
 } = require("../controllers/user_controllers");
 const passport = require("passport");
 
+// Created the router
 const router = express.Router();
 
+// Created router for doctor register
 router.post("/doctors/register", registerDoctor);
 
+// Created router for doctor login
 router.post("/login", login);
 
+// Created router for patient register
 router.post(
   "/patients/register",
   passport.authenticate("jwt", { session: false }),
   registerPatient
 );
 
+// Created router for patient report
 router.post(
   "/patients/:id/create_report",
   passport.authenticate("jwt", { session: false }),
   createReport
 );
 
+// created router for patient all report
 router.get("/patients/:id/all_reports", all_reports);
 
+// created router for particular patient status
 router.get("/reports/:status", AllReports);
 
 module.exports = router;
